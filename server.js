bd5346c57c662af6e87fafd71c0b13690c2f8e6f
Helen <sang.s.yi@gmail.com>
2021-05-21 20:46:47 -0700

    notes routes

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    let saveNote = JSON.parse(fs.readFileSync('./db/db.json');
+    res.json(saveNote[Number(req.params.id)])
+});
